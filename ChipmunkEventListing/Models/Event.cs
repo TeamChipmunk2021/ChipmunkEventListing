f927fde0f7365103068961498735da7951edef9b
DanConant <daniel.conant@sky.uk>
2022-01-07 14:57:06 +0000

    update

---
+++ ChipmunkEventListing/Models/Event.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ChipmunkEventListing.Models
+{
+    public class Event
+    {
+        public int ID { get; set; }
+        public string eventTitle { get; set; }
+        public string eventDescription { get; set; }
+        public string image_location { get; set; }
+
+
+        [DataType(DataType.Date)]
+        public DateTime start_date { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime end_date { get; set; }
+
+    }
+}
