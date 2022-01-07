79cce422dee4d3d4835c130d70290fd6f7ef78bc
Zizonella <buzasizita@gmail.com>
2022-01-07 15:31:48 +0000

    Added new models

---
+++ ChipmunkEventListing/Models/Act.cs
@@ -0,0 +1,10 @@
+﻿namespace ChipmunkEventListing.Models
+{
+    public class Act
+    {
+        public int ID;
+        public string ActName;
+        public int UserId;
+        public int GenreId;
+    }
+}
