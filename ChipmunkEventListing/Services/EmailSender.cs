3d26be3862c4b4affd2d54b4769ce18e4bfa650a
Zizonella <buzasizita@gmail.com>
2022-01-18 18:57:36 +0000

    Added email sending feature

---
+++ ChipmunkEventListing/Services/EmailSender.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace ChipmunkEventListing.Services
+{
+    public class EmailSender : IEmailSender
+    {
+        public EmailSender()
+        {
+
+        }
+
+        public async Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            string mail = "chipmunkseventlisting@gmail.com";
+            string password = "ChipmunkEvent2021!";
+
+            MailMessage message = new MailMessage();
+            message.From = new MailAddress(mail);
+            message.Subject = subject;
+            message.To.Add(new MailAddress(email));
+            message.Body = "<html><body> " + htmlMessage + " </body></html>";
+            message.IsBodyHtml = true;
+            // setting up the Smtp Client
+            var smtpClient = new SmtpClient("smtp.gmail.com")
+            {
+                Port = 587,
+                Credentials = new NetworkCredential(mail, password),
+                EnableSsl = true,
+            };
+            smtpClient.Send(message);
+        }
+
+    }
+}
--- ChipmunkEventListing/Startup.cs
+++ ChipmunkEventListing/Startup.cs
@@ -8,6 +8,9 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using ChipmunkEventListing.Services;
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -39,24 +42,26 @@ namespace ChipmunkEventListing
                     options.UseSqlServer(Configuration.GetConnectionString("EventContext")));
             services.Configure<IdentityOptions>(options =>
              {
-                // Password settings.
-                options.Password.RequireDigit = true;
+                 // Password settings.
+                 options.Password.RequireDigit = true;
                  options.Password.RequireLowercase = true;
                  options.Password.RequireNonAlphanumeric = true;
                  options.Password.RequireUppercase = true;
                  options.Password.RequiredLength = 6;
                  options.Password.RequiredUniqueChars = 1;
 
-                // Lockout settings.
-                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+                 // Lockout settings.
+                 options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
                  options.Lockout.MaxFailedAccessAttempts = 5;
                  options.Lockout.AllowedForNewUsers = true;
 
-                // User settings.
-                options.User.AllowedUserNameCharacters =
-                 "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+                 // User settings.
+                 options.User.AllowedUserNameCharacters =
+                  "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
                  options.User.RequireUniqueEmail = false;
              });
+            // registering the email sender
+            services.AddTransient<IEmailSender, EmailSender>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
