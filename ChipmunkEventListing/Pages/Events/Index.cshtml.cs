e42fb78b7bfc0cd09f39d772e260cf41d05a06a7
DanConant <daniel.conant@sky.uk>
2022-01-21 17:20:21 +0000

    Added pagination and search to the event page, updated layout formatting

--- ChipmunkEventListing/Pages/Events/Index.cshtml.cs
+++ ChipmunkEventListing/Pages/Events/Index.cshtml.cs
@@ -7,23 +7,78 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using ChipmunkEventListing.Data;
 using ChipmunkEventListing.Models;
+using Microsoft.Extensions.Configuration;
 
 namespace ChipmunkEventListing.Pages.Events
 {
     public class IndexModel : PageModel
     {
-        private readonly ChipmunkEventListing.Data.EventContext _context;
+        private readonly EventContext _context;
 
-        public IndexModel(ChipmunkEventListing.Data.EventContext context)
+        private readonly IConfiguration Configuration;
+        public IndexModel(EventContext context, IConfiguration configuration)
         {
             _context = context;
+            Configuration = configuration;
         }
 
-        public IList<Event> Event { get;set; }
+        public string EventNameSort { get; set; }
+        public string StartDateSort { get; set; }
+        public string CurrentFilter { get; set; }
+        public string CurrentSort { get; set; }
 
-        public async Task OnGetAsync()
+
+        public PaginatedList<Event> Events { get; set; }
+
+        public async Task OnGetAsync(string sortOrder,
+            string currentFilter, string searchString, int? pageIndex)
         {
-            Event = await _context.Events.ToListAsync();
+            CurrentSort = sortOrder;
+            EventNameSort = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            StartDateSort = sortOrder == "Date" ? "date_desc" : "Date";
+            if (searchString != null)
+            {
+                pageIndex = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+            // using System;
+            EventNameSort = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            StartDateSort = sortOrder == "Date" ? "date_desc" : "Date";
+
+            CurrentFilter = searchString;
+
+            IQueryable<Event> eventsIQ = from s in _context.Events
+                                       select s;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                eventsIQ = eventsIQ.Where(s => s.EventTitle.Contains(searchString));
+            }
+
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    eventsIQ = eventsIQ.OrderByDescending(s => s.EventTitle);
+                    break;
+                case "StartDate":
+                    eventsIQ = eventsIQ.OrderBy(s => s.StartDate);
+                    break;
+                case "date_desc":
+                    eventsIQ = eventsIQ.OrderByDescending(s => s.StartDate);
+                    break;
+                default:
+                    eventsIQ = eventsIQ.OrderBy(s => s.EventTitle);
+                    break;
+            }
+
+            var pageSize = Configuration.GetValue("PageSize", 4);
+            Events = await PaginatedList<Event>.CreateAsync(
+                eventsIQ.AsNoTracking(), pageIndex ?? 1, pageSize);
+
+            // Users = await usersIQ.AsNoTracking().ToListAsync();
         }
     }
 }
